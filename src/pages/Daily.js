aa7fd98c18880dbef2742ab364011764a7254610
Eom-sangji <usj1011@naver.com>
2025-08-21 15:07:49 +0900

    Daily 수정, CashList, CashItem 수정

--- src/pages/Daily.js
+++ src/pages/Daily.js
@@ -1,14 +1,25 @@
-import Button from "../component/Button";
-import Header from "../component/Header";
+import {useState} from "react";
+import Button from "../components/Button";
+import Header from "../components/Header";
 
 const Daily=()=>{
+    const[pivotDate, setPivotDate]=useState(new Date());
+    const headerTitle= `${pivotDate.getFullYear()}년 ${pivotDate.getMonth()+1}월`;
+
+    const onIncreaseMonth =()=>{
+        setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth()+1));
+    };
+    const onDecreaseMonth=()=>{
+        setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth()-1));
+    };
+    
     return(
         <div>
             <Header
-                title={"yyyy년 mm월"}
-                leftChild={<Button text={"<"}/>}
-                rightChild={<Button text={">"}/>}
-            />
+                title={headerTitle}
+                leftChild={<Button text={"<"} onClick={onDecreaseMonth}/>}
+                rightChild={<Button text={">"} onClick={onIncreaseMonth}/>}
+        />
         </div>
     );
 };
