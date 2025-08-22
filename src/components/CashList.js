499f4330e18672e1a0174523c14b7956fbb2b356
Eom-sangji <usj1011@naver.com>
2025-08-22 10:19:33 +0900

    cash, daily 수정

--- src/components/CashList.js
+++ src/components/CashList.js
@@ -1,37 +1,48 @@
-import {useState,useEffect} from "react";
-import Button from "./Button";
-import "./CashList.css";
-import {useNavigate} from "react-router-dom";
-import CashItem from './CashItem';
+ import {useState,useEffect} from "react";
+ import "./CashList.css";
+ import CashItem from './CashItem';
 
-const CashList = ({data})=>{
-   const navigate=useNavigate();
-    const [sortedData, setSortedData] = useState([]);
+ const CashList = ({data})=>{
+     const [sortedData, setSortedData] = useState([]);
 
-    useEffect(()=>{
-        const sortedList=[...data].reverse();
-        setSortedData(sortedList);
-    }, [data]);
+     useEffect(()=>{
+         const sortedList=[...data].reverse();
+         setSortedData(sortedList);
+     }, [data]);
 
-     const OnClickAdd=()=>{
-        navigate("/add");
-    };
+     return (
+     <div className="CashList">
+       <div className="menu_wrapper">
+         <div className="right_col">
+         </div>
+       </div>
 
-    return (
-    <div className="CashList">
-      <div className="menu_wrapper">
-        <div className="right_col">
-          <Button type={"positive"} text={"소비내역 등록"} onClick={OnClickAdd} />
-        </div>
-      </div>
+       <div className="list_wrapper">
+         {sortedData.map((it)=>(
+                     <CashItem key={it.id}{...it}/>
+                 ))}
+           </div>
+       </div>
+   );
+ };
 
-      <div className="list_wrapper">
-        {sortedData.map((it)=>(
-                    <CashItem key={it.id}{...it}/>
-                ))}
-          </div>
-      </div>
-  );
-};
+ export default CashList;
 
-export default CashList;
+// import "./CashList.css";
+// import CashItem from "./CashItem";
+
+// const CashList = ({ data }) => {
+//   if (!data || data.length === 0) {
+//     return <div>내역이 없습니다.</div>;
+//   }
+
+//   return (
+//     <div className="CashList">
+//       {data.map((it) => (
+//         <CashItem key={it.id} {...it} />
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default CashList;
